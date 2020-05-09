6b9efb6923befc9c128b14ca41e6e260a5f0a8a5
zhaofeng-shu33 <616545598@qq.com>
2020-05-09 15:12:58 +0800

    add generate questions

--- generate.py
+++ generate.py
@@ -1,8 +1,13 @@
 import os
+import argparse
+import json
 
 from bs4 import BeautifulSoup
 
 from question import process_question_from_url
+
+ANSWER_FOLDER = 'answer'
+QUESTION_FOLDER = 'questions'
 
 def get_soup_obj():
     if not os.path.exists('answer'):
@@ -24,9 +29,35 @@ def process_answerlist_item(item):
     contents = ''
     contents += '# ' + title + '\n'
     contents += createtime + '\n\n'
-    contents += process_question_from_url(url)
-    with open(os.path.join('answer', file_name), 'w') as f:
+    contents += process_question_from_url(url, ANSWER_FOLDER)
+    with open(os.path.join(ANSWER_FOLDER, file_name), 'w') as f:
         f.write(contents)
+
+def process_questionlist_item(item):
+    title = item['title']
+    if len(title) > 10:
+        title_short = title[:10]
+    else:
+        title_short = title
+    createtime = item['createTime']
+    url = '/question/' + item['qid']
+    file_name = createtime + '_' + title_short + '.md'
+    contents = ''
+    contents += '# ' + title + '\n'
+    contents += createtime + '\n\n'
+    contents += process_question_from_url(url, QUESTION_FOLDER)
+    with open(os.path.join(QUESTION_FOLDER, file_name), 'w') as f:
+        f.write(contents)
+   
+def generate_questions():
+    json_file_list = os.listdir('./')
+    for file_name in json_file_list:
+        if file_name.find('.json') < 0:
+            continue
+        f = open(file_name)
+        js = json.load(f)
+        for i in js['data']['question']['list']:
+            process_questionlist_item(i)
 
 def generate_answer():
     soup = get_soup_obj()
@@ -34,5 +65,12 @@ def generate_answer():
         process_answerlist_item(i)
 
 if __name__ == '__main__':
-    generate_answer()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--action', choices=['generate_answer', 'generate_questions'],
+        default='generate_answer')
+    args = parser.parse_args()
+    if args.action == 'generate_answer':
+        generate_answer()
+    elif args.action == 'generate_questions':
+        generate_questions()
 
--- question.py
+++ question.py
@@ -9,35 +9,10 @@ import shutil
 username = '丰赵'
 base_url = 'https://zhidao.baidu.com'
 
-def process_question_from_str(st):
+def process_question_from_str(st, folder):
     soup = BeautifulSoup(st)
     if soup.title.text.find('信息提示') > 0:
         return 'deleted'
-
-    # whether I am the best replier
-    repliers = soup.find_all(class_='wgt-replyer-all-card')
-    # find my replier index    
-    index = 0
-    for i in repliers:
-        if i.text.find(username) > 0:
-            break
-        index += 1
-    if index == len(repliers):
-        return ''
-    if index == 0:
-        my_answer = soup.find(class_='best-text')
-        if my_answer is None:
-            my_answer = soup.find(class_='answer-text')
-    else:
-        index -= 1
-        my_answer = soup.find_all(class_='answer-text')[index]
-    my_answer_text = my_answer.text.replace('展开全部','')
-    my_answer_text = my_answer_text.replace('\n\n\n','')
-    answer_img = my_answer.find('img')
-    if answer_img:
-        img_url = answer_img['src'].split('?')[0]
-        img_name = download_image(img_url, 'answer')
-        my_answer_text += '\n![](%s)\n' % img_name
     try:
         question = soup.find(class_='q-content').find_all('span')[1]
     except Exception as e:
@@ -53,6 +28,36 @@ def process_question_from_str(st):
                 question_text += '\n![](%s)\n' % img_name
     except AttributeError:
         pass
+
+    # whether I am the best replier
+    repliers = soup.find_all(class_='wgt-replyer-all-card')
+    # find my replier index    
+    index = 0
+    for i in repliers:
+        if i.text.find(username) > 0:
+            break
+        index += 1
+
+    if index == len(repliers):
+        my_answer = soup.find(class_='best-text') # I do not answer this question
+    elif index == 0:
+        my_answer = soup.find(class_='best-text')
+        if my_answer is None:
+            my_answer = soup.find(class_='answer-text')
+    else:
+        index -= 1
+        my_answer = soup.find_all(class_='answer-text')[index]
+
+    if my_answer is None:
+        my_answer_text = ''
+    else:
+        my_answer_text = my_answer.text.replace('展开全部','')
+        my_answer_text = my_answer_text.replace('\n\n\n','')
+        answer_img = my_answer.find('img')
+        if answer_img:
+            img_url = answer_img['src'].split('?')[0]
+            img_name = download_image(img_url, folder)
+            my_answer_text += '\n![](%s)\n' % img_name
     return question_text + '\n\n' + my_answer_text
 
 def download_image(img_url, folder):
@@ -73,12 +78,12 @@ def download_image(img_url, folder):
     shutil.copy(file_name, new_file)
     return img_name
 
-def process_question_from_file(file_name):
+def process_question_from_file(file_name, folder):
     with open(file_name) as f:
         st = f.read()
-    return process_question_from_str(st)
+    return process_question_from_str(st, folder)
  
-def process_question_from_url(url):
+def process_question_from_url(url, folder):
     question_id = url.split('/')[-1]
     file_name = os.path.join('build', question_id + '.html')
     if not os.path.exists(file_name):
@@ -89,5 +94,5 @@ def process_question_from_url(url):
         r.encoding = 'gbk'
         with open(file_name, 'w') as f:
             f.write(r.text)
-    return process_question_from_file(file_name)
+    return process_question_from_file(file_name, folder)
     
